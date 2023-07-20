93015c541bb05c3790f169cc76c5e23ff7aaea56
Ala Rmida <ala.rmida.98@gmail.com>
2023-07-20 16:38:39 +0100

    Raspberry pi basic example with C programming Language

---
+++ 1.1.1_BlinkingLed.c
@@ -0,0 +1,30 @@
+/**********************************************************************
+
+* Author      : Ala Rmida 
+* E-mail      : ala.rmida.ingen@gmail.com
+* website     : https://ala-rmida-ann1998.netlify.app/
+**********************************************************************/
+#include <wiringPi.h>  
+#include <stdio.h>
+#define LedPin      0
+int main(void)
+{
+    // When initialize wiring failed, print message to screen
+    if(wiringPiSetup() == -1){
+        printf("setup wiringPi failed !");
+        return 1;
+    }
+    pinMode(LedPin, OUTPUT);// Set LedPin as output to write value to it.
+    while(1){
+        // LED on
+        digitalWrite(LedPin, LOW);
+        printf("...LED on\n");
+        delay(500);
+        // LED off
+        digitalWrite(LedPin, HIGH);
+        printf("LED off...\n");
+        delay(500);
+    }
+    return 0;
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# Raspberry-pi-c-programs
+In this Repository you can find some of the most my programs application with c programming language on Raspberry pi  
